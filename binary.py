c19de63cac6b501d96c8ae89913196aabed5af1d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-27 18:14:11 +0000

    Add files via upload

--- binary.py
+++ binary.py
@@ -9,20 +9,22 @@ class bins:
     def writes(self):
         names=self.names
         sizes1=int(self.sizes//4096)
+        print(sizes1)
         sizes2=int(self.sizes-(sizes1*4096))
+        print(sizes2)
         self.returns=self.bytes[:sizes2]
         f1=open(names,"wb")
-        if sizes2>0:
+        if sizes1>0:
             for n in range(sizes1):
             
                 f1.write(self.bytes)
-        if sizes1>0:
+        if sizes2>0:
             f1.write(self.returns)
         f1.close()
 c=input("give me file name ? ")
 a=input("give me size in bytes ? ") 
 b=bins()
 b.sizes=int(a)
-b.names=c
+#b.names=c
 
 b.writes()
