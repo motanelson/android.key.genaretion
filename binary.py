3b9b2092e0f640a0e29c6041c2ad5876b641c445
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-27 17:29:07 +0000

    Add files via upload

--- binary.py
+++ binary.py
@@ -2,26 +2,27 @@ print("\033c\033[43;30m\n")
 
 class bin:
     def __init__(self):
-        self.bytes=b''
+        self.bytes=b'\0'*4096
+        self.returns=b''
         self.sizes=0
         self.names="file.dat"
-    def creates(self):
-        sizes=self.sizes
-        
-        zero=0
-        self.bytes=b''
-        for n in range(sizes):
-            self.bytes=self.bytes+b'\0'
     def writes(self):
         names=self.names
-        
+        sizes1=int(self.sizes//4096)
+        sizes2=int(self.sizes-(sizes1*4096))
+        self.returns=self.bytes[:sizes2]
         f1=open(names,"wb")
-        f1.write(self.bytes)
+        if sizes2>0:
+            for n in range(sizes1):
+            
+                f1.write(self.bytes)
+        if sizes1>0:
+            f1.write(self.returns)
         f1.close()
 c=input("give me file name ? ")
 a=input("give me size in bytes ? ") 
 b=bin()
 b.sizes=int(a)
 b.names=c
-b.creates()
+
 b.writes()
