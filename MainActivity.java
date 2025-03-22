04aa9a21fd13335f69b9aa7666c623b358272580
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-03-22 07:55:29 +0000

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -10,7 +10,7 @@ import android.os.*;
 import android.app.AlertDialog;
 import android.app.Activity;
 import android.widget.Toast;
-
+import java.util.Random;
 
 public class MainActivity extends Activity
 {
@@ -21,9 +21,20 @@ public class MainActivity extends Activity
     private final Runnable updateTimeRunnable = new Runnable() {
         
         public void run() {
-            String currentTime = dateFormat.format(new Date());
-            textView.setText(currentTime);
-            handler.postDelayed(this, 1000); // Atualiza a cada segundo
+            Random r = new Random();
+            char c=' ';
+            String ss ="0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz";
+            int n=0;
+            int i =r.nextInt(36);
+            String s = "";
+            for(n=0;n<19;n++){
+                     i =r.nextInt(36);
+                     c=ss.charAt(i);
+                     s=s+c;
+                     
+            }
+            textView.setText(s);
+            handler.postDelayed(this, 20000); // Atualiza a cada segundo
         }
     };
     
